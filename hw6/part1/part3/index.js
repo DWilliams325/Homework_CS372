3b6e266062892799f2659acdfea5d6b1bfb590b5
Damian Williams <damianwil325@yahoo.com>
2025-10-07 22:28:09 -0400

    hw6 part1,2,3

---
+++ hw6/part1/part3/index.js
@@ -0,0 +1,21 @@
+function initialize() {
+  const fenway = { lat: 41.0793, lng: -85.1394 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: fenway,
+    zoom: 14,
+  });
+  const panorama = new google.maps.StreetViewPanorama(
+    document.getElementById("pano"),
+    {
+      position: fenway,
+      pov: {
+        heading: 34,
+        pitch: 10,
+      },
+    },
+  );
+
+  map.setStreetView(panorama);
+}
+
+window.initialize = initialize;
